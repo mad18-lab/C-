12511c03ed639921d57670aac0623e021f1a2ce1
mad18-lab <80634128+mad18-lab@users.noreply.github.com>
2022-08-10 00:25:01 +0530

    Update My Diary.py

--- My Diary.py
+++ My Diary.py
@@ -1,5 +1,5 @@
-import textwrap
-count = 5
+import textwrap             # module-to-display-output-as-paragraph
+count = 5                   # number-of-tries-to-guess-password
 while count != 0:
     pwd = input("Please enter password to access: ")
     if pwd == "your name":
@@ -9,29 +9,29 @@ while count != 0:
         print("\n*****MAIN MENU*****")
         print("1. Write a new entry(s)")
         print("2. Access a previous entry")
-        print("3. Add an entry to a previous date")
-        print("4. Delete a previous entry")
-        print("5. Print all entries")
-        print("6. Delete all entries")
+        print("3. Delete a previous entry")
+        print("4. Print all entries")
+        print("5. Delete all entries")
 
 
-        def new_entry():
-            diary = open("diary.txt", "a")
-            n = int(input("\nDo you want to make a single entry or more than one (answer in number): "))
-            if n > 1:
-                for i in range(n):
+        def new_entry():        # function-to-write-new-entries
+            num = int(input("\nDo you want to make a single entry or multiple entries: "))
+            if num > 1:
+                for i in range(num):
+                    diary = open("diary.txt", "a")
                     diary.write("\nYour Entry: ")
-                    entry = input("\nEnter the date and your corresponding entry: ")
-                    diary.write(entry + '\n')
-
+                    date = input("\nEnter your date: ")
+                    entry = input("\nEnter your entry: ")
+                    diary.write(date + " - " + entry + "\n")
             else:
+                diary = open("diary.txt", "a")
                 diary.write("\nYour Entry: ")
-                entry = input("\nEnter the date and your corresponding entry: ")
-                diary.write(entry + '\n')
-                diary.close()
+                date = input("\nEnter your date: ")
+                entry = input("\nEnter your entry: ")
+                diary.write(date + " - " + entry + "\n")
 
 
-        def read_entry():
+        def read_entry():       # function-to-read-a-specific-entry
             diary = open("diary.txt", "r")
             lines = diary.readlines()
             entries = []
@@ -69,72 +69,7 @@ while count != 0:
             diary.close()
 
 
-        def add_entry():
-            diary = open("diary.txt", "r+")
-            lines = diary.readlines()
-            entries = []
-            print("\n(Psst. If you cannot remember a certain date or keyword of an entry, "
-                  "just type can't remember or don't remember)")
-            date = input("\nEnter date of entry you wish to modify: ")
-
-            if date == "can't remember" or date == "don't remember":
-                key_word = input("\nThat's okay! Enter specific keyword(s) of entry you wish to modify: ")
-                if key_word == "can't remember" or key_word == "don't remember":
-                    print("\nHere are all your entries to alleviate your confusion: ")
-                    all_entry()
-                else:
-                    for line in lines:
-                        if key_word in line:
-                            entries.append(line)
-                        else:
-                            continue
-
-                    for i in range(len(entries)):
-                        output3 = textwrap.fill(entries[i], width=125)
-                        print(output3)
-
-                    rep1 = "\nEnter the portion of the entry you want to succeed: "
-                    mod1 = "\nEnter your successive entry: "
-
-                    if rep1 in entries[-1]:
-                        entries.append(mod1)
-                    else:
-                        for i, line in enumerate(entries):
-                            if rep1 in line and mod1 not in entries[i + 1]:
-                                entries.insert(i + 1, mod1)
-                                break
-                    diary.seek(0)
-                    diary.writelines(entries)
-
-
-            else:
-                for line in lines:
-                    if date in line:
-                        entries.append(line)
-                    else:
-                        continue
-
-                for i in range(len(entries)):
-                    output4 = textwrap.fill(entries[i], width=125)
-                    print(output4)
-
-                rep2 = input("\nEnter the portion of the entry you want to succeed: ")
-                mod2 = input("\nEnter your successive entry: ")
-
-                if mod2 in lines[-1]:
-                    lines.append(mod2)
-
-                else:
-                    for i, line in enumerate(lines):
-                        if rep2 in line and mod2 not in lines[i + 1]:
-                            lines.insert(i + 1, mod2)
-                            break
-
-                diary.seek(0)
-                diary.writelines(lines)
-
-
-        def del_entry():
+        def del_entry():        # function-to-delete-a-specific-entry
             diary = open("diary.txt", "r")
             lines = diary.readlines()
             entries = []
@@ -188,7 +123,7 @@ while count != 0:
                 del_diary.close()
 
 
-        def all_entry():
+        def all_entry():        # function-to-print-all-entries
             diary = open("diary.txt", "r")
             lines = diary.readlines()
             for line in lines:
@@ -196,30 +131,27 @@ while count != 0:
                 print(read)
 
 
-        def clear_entry():
+        def clear_entry():      # function-to-delete-all-entries
             diary = open("diary.txt", "r+")
             diary.truncate()
             print("\nAll entries successfully deleted. Thank you!")
             diary.close()
 
 
-        choice = int(input("\nEnter Your Choice: "))
-        if choice == 1:
+        choice = int(input("\nEnter Your Choice: "))        # choice-for-main-menu
+        if choice == 1:                                     # calling-functions
             new_entry()
 
         elif choice == 2:
             read_entry()
 
         elif choice == 3:
-            add_entry()
-
-        elif choice == 4:
             del_entry()
 
-        elif choice == 5:
+        elif choice == 4:
             all_entry()
 
-        elif choice == 6:
+        elif choice == 5:
             clear_entry()
 
         else:
@@ -237,6 +169,7 @@ while count != 0:
         count = count - 1
         if count == 0:
             print("\nWrong password. You are not allowed access to the diary. Please try later.")
+            print("\nA hint: your password is your name.")
         else:
             print("\nWrong password. Access Denied. Try again.")
             print("You have", count, "tries left.")
